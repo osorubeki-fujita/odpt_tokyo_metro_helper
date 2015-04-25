e827d6f8f1f96b2ff54c4e09bb10a7872c67181c
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-25 20:17:01 +0900

    20150425_201701 Install Gem (0.1.8)

--- lib/odpt_tokyo_metro_helper/string_ext.rb
+++ lib/odpt_tokyo_metro_helper/string_ext.rb
@@ -5,8 +5,10 @@ module OdptTokyoMetroHelper::StringExt
     str.process_kouji
   end
 
-  private
+  protected
 
+  # 「麹」を機種依存文字「麴」に変換するメソッド
+  # @return [String]
   def process_kouji
     gsub( ::OdptTokyoMetroHelper::RegexpLibrary.kojimachi , "麴町" )
   end
--- spec/odpt_tokyo_metro_helper_spec.rb
+++ spec/odpt_tokyo_metro_helper_spec.rb
@@ -1,12 +1,20 @@
+# -*- coding: utf-8 -*-
+
 require 'spec_helper'
 require 'deployer'
 
 spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
-version = "0.1.7"
+version = "0.1.8"
 
 describe OdptTokyoMetroHelper do
   it "has a version number \'#{ version }\'" do
     expect( ::OdptTokyoMetroHelper::VERSION ).to eq( version )
     expect( ::Deployer.version_check( ::OdptTokyoMetroHelper::VERSION , spec_filename ) ).to eq( true )
   end
+
+  it "has a method \#process_specific_letter" do
+    expect( "池袋".process_specific_letter ).to eq( "池袋" )
+    expect( "麹町".process_specific_letter ).to eq( "麴町" )
+  end
+
 end
